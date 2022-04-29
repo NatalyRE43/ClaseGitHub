b4f39e846130b683944f354aebdc0b627a83a6cc
Lucas Bianco <103225865+HebrineX@users.noreply.github.com>
2022-04-28 21:02:12 -0300

    Objeto Calculadora

---
+++ CalcObj.js
@@ -0,0 +1,10 @@
+let calculadora = {
+    suma : (n1,n2)=>n1+n2,
+    
+    resta :(n1,n2)=>n1-n2,
+
+    multiplicacion :(n1,n2)=>n1*n2,
+
+    division :(n1,n2)=>n1/n2,
+}
+module.export = calculadora;
